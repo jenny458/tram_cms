b1c4cdd78ee3200369ad987fe24fbbc3ada7f5d2
zomnao <ankhemanattrattanaphurithanakul@fico.com>
2021-02-01 16:40:16 +0700

    keep authen in localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from './services/local-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,27 @@ export class AppComponent {
   password = "";
   isLogin = false;
 
+  constructor(
+    private localStorageService: LocalStorageService
+  ){
+    let localIsLogin: boolean = this.localStorageService.getOnLocalStorage();
+    let localTime: number = this.localStorageService.getOnLocalTimeStampStorage();
+    if(localIsLogin && (new Date().getTime() - localTime)<3600000){
+      this.isLogin = true;
+    }else{
+      this.isLogin = false;
+    }
+  }
+
   login(){
     if(this.username === "admin" && this.password === "password!@#"){
-        this.isLogin = true;
+      this.isLogin = true
+      this.localStorageService.storeOnLocalStorage(this.isLogin);
     }
   }
 
   logout(){
     this.isLogin = false;
+    this.localStorageService.storeOnLocalStorage(this.isLogin);
   }
 }
